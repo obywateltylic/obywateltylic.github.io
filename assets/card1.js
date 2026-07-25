f647ee343423fb90aa39114ac8a2e7905522ea5a
obywateltylic <szymonobiblioteka@gmail.com>
2026-07-26 01:05:54 +0200

    Update card1.js

--- assets/card1.js
+++ assets/card1.js
@@ -17,53 +17,47 @@ function setClock() {
 
 
 
-function loadData() {
+async function loadData() {
   const data = Object.fromEntries(params);
 
+  const db = await getDb();
+  const saved = await getData(db, "data");
 
-  set("name", data.name.toUpperCase());
-  set("surname", data.surname.toUpperCase());
 
-  const birthday = `${data.day}.${data.month}.${data.year}`;
-  set("birthday", birthday);
+  set("name", saved.name.toUpperCase());
+  set("surname", saved.surname.toUpperCase());
+  set("birthday", saved.birthday);
+  set("pesel", saved.pesel);   
 
-  set("pesel", data.pesel);
-
+  
   const legit = generateLegitNumber();
   set("legitNumber", legit);
 
-
+ 
 
   const today = new Date();
   let schoolYear;
 
- 
   if (today.getMonth() >= 8) {
     schoolYear = today.getFullYear();
   } else {
-
     schoolYear = today.getFullYear() - 1;
   }
 
-
   const givenDateObj = new Date(schoolYear, 8, 25);
-  const givenDate = givenDateObj.toLocaleDateString("pl-PL");
-
-
   const expiryDateObj = new Date(schoolYear + 1, 8, 30);
-  const expiryDate = expiryDateObj.toLocaleDateString("pl-PL");
 
-  set("givenDate", givenDate);
-  set("expiryDate", expiryDate);
+  set("givenDate", givenDateObj.toLocaleDateString("pl-PL"));
+  set("expiryDate", expiryDateObj.toLocaleDateString("pl-PL"));
 
 
-  set("school", data.school);
-  set("class", data.class);
-  set("schoolAddress", data.schoolAddress);
-  set("schoolPhone", data.schoolPhone);
-  set("director", data.director);
+
+  set("school", data.school || "");
+  set("class", data.class || "");
+  set("schoolAddress", data.schoolAddress || "");
+  set("schoolPhone", data.schoolPhone || "");
+  set("director", data.director || "");
 }
-
 
 
 function set(id, value) {
@@ -82,11 +76,39 @@ function generateLegitNumber() {
 }
 
 
-
 function loadImage() {
   const url = params.get("image");
   if (!url) return;
 
   const img = document.querySelector(".id_own_image");
   img.style.backgroundImage = `url(${url})`;
+}
+
+
+
+function getDb() {
+  return new Promise((resolve, reject) => {
+    var request = window.indexedDB.open("cwelObywatel", 1);
+    request.onerror = (event) => reject(event.target.error);
+
+    request.onupgradeneeded = (event) => {
+      var db = event.target.result;
+      if (!db.objectStoreNames.contains("data")) {
+        db.createObjectStore("data", { keyPath: "data" });
+      }
+    };
+
+    request.onsuccess = (event) => {
+      resolve(event.target.result);
+    };
+  });
+}
+
+function getData(db, name) {
+  return new Promise((resolve, reject) => {
+    var store = db.transaction("data", "readonly").objectStore("data");
+    var request = store.get(name);
+    request.onsuccess = () => resolve(request.result || {});
+    request.onerror = (event) => reject(event.target.error);
+  });
 }
